9245f141d29373a5c705535dea3fcd221eae436d
055055 <snb4301@gmail.com>
2021-12-28 16:59:01 +0900

    HTMl Page Rendering 로직 분리 - WEbConfig addViewControllers

--- src/main/java/com/smile/prebookingforgames/controller/EventController.java
+++ src/main/java/com/smile/prebookingforgames/controller/EventController.java
@@ -1,13 +1,12 @@
 package com.smile.prebookingforgames.controller;
 
-import com.smile.prebookingforgames.dto.CouponListDTO;
 import com.smile.prebookingforgames.dto.CouponIssueDto;
+import com.smile.prebookingforgames.dto.CouponListDTO;
 import com.smile.prebookingforgames.service.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,15 +17,10 @@ import java.util.Map;
 
 @Slf4j
 @RequestMapping("/api/events")
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
-
-    @GetMapping
-    public String forwardCouponIssuePage() {
-        return "register";
-    }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity issueCoupon(@RequestBody @Valid CouponIssueDto couponIssueDto) {
@@ -34,14 +28,8 @@ public class EventController {
         return ResponseEntity.ok().body(result);
     }
 
-    @GetMapping(value = "/coupon-list")
-    public String forwardCouponListPage() {
-        return "coupon-list";
-    }
-
     @GetMapping(value = "/coupon-list/all")
-    public @ResponseBody
-    Map<String, List> couponListAll() {
+    public Map<String, List> couponListAll() {
         List<CouponListDTO> result = eventService.getCouponList();
         Map<String, List> data = new HashMap<>();
         data.put("data", result);
